0c6f67ff0d69da4ea4618d6e1a0d54345340ce6a
bitlin <bitlin@outlook.com>
2017-10-09 18:11:25 +0800

    create an Intent and then start the other activity
    Signed-off-by: bitlin <bitlin@outlook.com>

--- TwoActivities_myMessenger/app/src/main/java/bitlin/example/android/twoactivities_mymessenger/CreateMessageActivity.java
+++ TwoActivities_myMessenger/app/src/main/java/bitlin/example/android/twoactivities_mymessenger/CreateMessageActivity.java
@@ -1,6 +1,7 @@
 package bitlin.example.android.twoactivities_mymessenger;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
@@ -14,6 +15,11 @@ public class CreateMessageActivity extends Activity {
 
     //Call onSendMessage() when the button is clicked
     public void onSendMessge(View view){
-        
+//  Whenever you want an activity to start a second activity, you use an intent. You can think of an intent as an “intent to do something.”
+//  It’s a type of message that allows you to bind separate objects (such as activities) together at runtime.
+//  If one activity wants to start a second activity, it does it by sending an intent to Android.
+//  Android will then start the second activity and pass it the intent.
+        Intent intent=new Intent(this, ReceiveMessageActivity.class);
+        startActivity(intent);
     }
 }
