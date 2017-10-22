3e76785bc8145c1cca771e9ff1047daf19b3a17a
bitlin <bitlin@outlook.com>
2017-10-22 12:51:18 +0800

    now we can get the details page by clicking a drink item in drink category page
    Signed-off-by: bitlin <bitlin@outlook.com>

--- Starbuzz/app/src/main/java/bitlin/example/android/starbuzz/DrinkActivity.java
+++ Starbuzz/app/src/main/java/bitlin/example/android/starbuzz/DrinkActivity.java
@@ -2,6 +2,8 @@ package bitlin.example.android.starbuzz;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class DrinkActivity extends Activity {
 
@@ -13,5 +15,26 @@ public class DrinkActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_drink);
+
+//     When the detail activity is started, it can retrieve the extra information from the intent and use it to populate its views.
+        int drinkId = (Integer)getIntent().getExtras().get(EXTRA_DRINKID);
+
+//        In our case, we can use drinkId to get details of the drink the user selected. drinkId is the ID of the drink,
+// the index of the drink in the drinks array. This means that you can get details about the drink the user clicked on using:
+        Drink drink = Drink.drinks[drinkId];
+// This gives us a Drink object containing all the information we need to update the views attributes in the activity
+
+        //Populate the drink name
+        TextView name = (TextView)findViewById(R.id.name);
+        name.setText(drink.getName());
+
+        //Populate the drink description
+        TextView description = (TextView)findViewById(R.id.description);
+        description.setText(drink.getDescription());
+
+        //Populate the drink image
+        ImageView photo = (ImageView)findViewById(R.id.photo);
+        photo.setImageResource(drink.getImageResourceId());
+        photo.setContentDescription(drink.getName());
     }
 }
