245f3fc26d8fe3b4a6b17d39e7e4eb48cf42ed0b
bitlin <bitlin@outlook.com>
2017-10-25 19:30:24 +0800

    enable to return to MainActivity from OrderActivity through clicking the back button
    Signed-off-by: bitlin <bitlin@outlook.com>

--- Bits_Pizzas/app/src/main/java/bitlin/example/android/bits_pizzas/OrderActivity.java
+++ Bits_Pizzas/app/src/main/java/bitlin/example/android/bits_pizzas/OrderActivity.java
@@ -1,5 +1,6 @@
 package bitlin.example.android.bits_pizzas;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -14,5 +15,11 @@ public class OrderActivity extends AppCompatActivity {
         Toolbar toolbar=(Toolbar)findViewById(R.id.toolbar);
 //      Set the toolbar as the activity's app bar
         setSupportActionBar(toolbar);
+
+//        This returns an object of type ActionBar.
+        ActionBar actionBar=getSupportActionBar();
+//        This enables the Up button.
+        actionBar.setDisplayHomeAsUpEnabled(true);
+
     }
 }
