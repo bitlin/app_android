e9628ed4efa9c5ccda6745711e57426582ad27ee
bitlin <bitlin@outlook.com>
2017-10-25 18:44:26 +0800

    to add “Create Order” button to the app bar, firstly  prepares the OrderActivity
    Signed-off-by: bitlin <bitlin@outlook.com>

---
+++ Bits_Pizzas/app/src/main/java/bitlin/example/android/bits_pizzas/OrderActivity.java
@@ -0,0 +1,18 @@
+package bitlin.example.android.bits_pizzas;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+public class OrderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order);
+
+        Toolbar toolbar=(Toolbar)findViewById(R.id.toolbar);
+//      Set the toolbar as the activity's app bar
+        setSupportActionBar(toolbar);
+    }
+}
