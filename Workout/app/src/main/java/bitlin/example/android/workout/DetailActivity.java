9a6f8d8f374ee9bd4d8a78ac9bf2c4e3f430701e
bitlin <bitlin@outlook.com>
2017-10-31 18:24:15 +0800

    WorkoutDetailFragment working process (steps)
    Signed-off-by: bitlin <bitlin@outlook.com>

--- Workout/app/src/main/java/bitlin/example/android/workout/DetailActivity.java
+++ Workout/app/src/main/java/bitlin/example/android/workout/DetailActivity.java
@@ -11,3 +11,14 @@ public class DetailActivity extends AppCompatActivity {
         setContentView(R.layout.activity_detail);
     }
 }
+
+//1. When the app is launched, activity MainActivity gets created. The user clicks on the button in MainActivity to start DetailActivity
+//2. DetailActivity’s onCreate() method runs. The onCreate() method specifies that activity_detail.xml should be used for DetailActivity’s layout.
+//3. activity_detail.xml sees that it includes a <fragment> element that refers to WorkoutDetailFragment.
+//4. WorkoutDetailFragment’s onCreateView() method is called.
+//   The onCreateView() method specifies that fragment_workout_detail.xml should be used for WorkoutDetailFragment’s layout.
+//   It inflates the layout to a View object.
+//5. activity_detail.xml’s Views are inflated to View Java objects.
+//    DetailActivity layout uses WorkoutDetailFragment’s View object in place of the <fragment> element in its layout’s XML.
+//6. Finally, DetailActivity is displayed on the device.
+//   Its layout contains the fragment WorkoutDetailFragment
